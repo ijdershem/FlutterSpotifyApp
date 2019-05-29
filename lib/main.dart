56ec4c8dc4bf7182bcad2acdf6cb1d55747d3dd9
ijdershem <ian.dershem@gmail.com>
2019-05-29 19:37:21 -0400

    User Authorization Token working

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,23 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:core';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:spotify/spotify_io.dart';
 import 'package:random_string/random_string.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart';
 import 'dart:convert' as JSON;
+import 'dart:convert';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
-var client_id = '80a3298daea74e078d240a508afcc4c1';
+var client_id;
 var client_secret;
-var redirect_uri;
+var redirect_uri = "http://localhost:3000/callback";
+var localhost;
+
 
 class MyApp extends StatelessWidget {
   @override
@@ -30,14 +36,15 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class BodyWidget extends StatefulWidget {
   @override
   BodyWidgetState createState() {
     return new BodyWidgetState();
   }
 }
+
 class BodyWidgetState extends State<BodyWidget> {
-  String host = "http://10.0.2.2:3000";
   String serverResponse = 'Server response';
 
   @override
@@ -53,8 +60,9 @@ class BodyWidgetState extends State<BodyWidget> {
             children: <Widget>[
               RaisedButton(
                 child: Text('Log into Spotify'),
-                onPressed: () {
-                  _makeGetRequest();
+                onPressed: () async { 
+                  final Token user_token = await _getToken();
+                  print(user_token.access_token);
                 },
               ),
               Padding(
@@ -68,28 +76,52 @@ class BodyWidgetState extends State<BodyWidget> {
     );
   }
 
-  Future _makeGetRequest() async {
-    host = _localhost();
-    Response response = await get(host);
+  Future<Token> _getToken() async {
+    Stream<String> onCode = await _server();
+    _auth();
+    final String code = await onCode.first;
+    print(code);
 
-    setState(() {
-      serverResponse = response.body;
-    });
-    _auth(host);
-  }
+    Response token_response = await post(
+      'https://accounts.spotify.com/api/token',
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + base64.encode(utf8.encode(new StringBuffer(client_id + ':' + client_secret).toString()))
+      },
+      body: { 
+        'grant_type' : "authorization_code",
+        'code' : code,
+        'redirect_uri' : redirect_uri
+      }
+    );
+    // print('Response status: ${token_response.statusCode}');
+    // print('Response body: ${token_response.body}');
+    return new Token.fromMap(JSON.jsonDecode(token_response.body));
+  } 
 
-  String _localhost() {
-    if (Platform.isAndroid)
-      return 'http://10.0.2.2:3000';
-    else // for iOS simulator
-      return 'http://localhost:3000';
+  Future<Stream<String>> _server() async {
+    final StreamController<String> onCode = new StreamController();
+    HttpServer server =
+      await HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 3000);
+      server.listen((HttpRequest request) async {
+        final String code = request.uri.queryParameters["code"];
+        request.response
+          ..statusCode = 200
+          ..headers.set("Content-Type", ContentType.HTML.mimeType)
+          ..write("<html><h1>Logged into Spotify</h1></html>");
+        await request.response.close();
+        await server.close(force: true);
+        onCode.add(code);
+        await onCode.close();
+      });
+    localhost = server;
+    return onCode.stream;
   }
 }
 
-void _auth(String host) async { 
+void _auth() async { 
   var state = randomAlphaNumeric(16);
   var scope = 'user-read-private user-read-email';
-  redirect_uri = host + "/callback";
   
   final Map<String, String> authQueryParameters = {
     "client_id": client_id,
@@ -112,3 +144,20 @@ void _auth(String host) async {
     throw 'Could not launch $url';
   }
 }
+
+class Token {
+  final String access_token;
+  final String token_type;
+  final String scope;
+  final num expires_in;
+  final String refresh_token;
+
+  Token(this.access_token, this.token_type, this.scope, this.expires_in, this.refresh_token);
+  Token.fromMap(Map<String, dynamic> json)
+        : access_token = json['access_token'],
+          token_type = json['token_type'],
+          scope = json['scope'],
+          expires_in = json['expires_in'],
+          refresh_token = json['refresh_token'];
+}
+
